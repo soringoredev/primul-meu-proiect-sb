6baf8bcb0359ec23de945572e66dacdb79196c41
Sorin Gore <soringoredev@gmail.com>
2025-05-14 22:22:23 +0300

    Last changes

--- src/main/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestController.java
+++ src/main/java/com/pixelacademy/springdemo_app/rest/HelloWorldRestController.java
@@ -34,7 +34,57 @@ public class HelloWorldRestController {
 
     @GetMapping("/meniu")
     public String meniu() {
-        return "Placinte cu brinza!";
+       paste();
+       sarmale();
+       supa();
+
+       String a = "\nPaste" + " papusoi";
+       String b = "Nu";
+
+
+        return "Meniu data 14 mai 2025! <br> <br>" + supa() +
+                " <br> " + sarmale() +
+                " <br> " + paste() ;
     }
+
+    @Override
+    public String toString() {
+        return "Geeks object => {" +
+
+                "felul 1 ='" + paste() + '\'' +
+                ",felul 2 ='" + supa() + '\''+
+                ",felul 3 ='" + sarmale() + '\''+
+                '}'
+                ;
+    }
+
+    public static void pizza() {
+        System.out.println("Pizza");
+    }
+
+    static void sushi() {
+        System.out.println("Sushi");
+    }
+
+    static void kebab() {
+        System.out.println("Kebab");
+    }
+
+
+
+    @GetMapping("/paste")
+   public static String paste() {
+        return "Paste bolognese!";
+    }
+    @GetMapping("/sarm")
+   public static String sarmale() {
+        return "Sarmale moldovenesti!";
+    }
+
+    @GetMapping("/supa")
+   public static String supa() {
+        return "Supa de SECRET!";
+    }
+
 
 }
